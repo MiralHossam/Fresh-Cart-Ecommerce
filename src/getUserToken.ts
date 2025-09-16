f82da14c1668de9b0d002a759ea234b73f54559a
MiralHossam <Miral.Hossam.22230117@commerce.helwan.edu.eg>
2025-09-16 16:23:42 +0300

    change cookies name

--- src/getUserToken.ts
+++ src/getUserToken.ts
@@ -1,24 +1,12 @@
-// 'use server'
-// import { cookies } from "next/headers";
-// import { decode } from "next-auth/jwt"
-
-// export async function getUserToken() {
-//   const cookieData = await cookies();
-//   const encryptedToken = cookieData.get("next-auth.session-token")?.value 
-//   const data = await decode({token: encryptedToken, secret: process.env.NEXTAUTH_SECRET!})
-//   return data?.token
-// }
-
-
 'use server'
 import { cookies } from "next/headers";
 import { decode } from "next-auth/jwt";
 
 export async function getUserToken() {
+  const TokenSession = {process.env.Node_ENV === "production"? '__Secure-next-auth.session-token':'next-auth.session-token'}
   const cookieData = await cookies();
   const encryptedToken =
-    cookieData.get("next-auth.session-token")?.value ||
-    cookieData.get("__Secure-next-auth.session-token")?.value;
+    cookieData.get(TokenSession)?.value;
 
   if (!encryptedToken) return null;
 
